798e040edaf198257472bd3d9b0a37d6ba978cfb
Berk Alp Yakici <berkalp.y@gmail.com>
2019-10-30 19:18:40 -0500

    Delete unnecessary loops.

--- modules/config.js
+++ modules/config.js
@@ -11,20 +11,13 @@ module.exports = {
         const directoryPath = path.join(__dirname, 'configs');
 
         let promise = new Promise((res, rej) => {
-            fs.readdir(directoryPath, function (err, files) {
-                var configs = [];
-    
+            fs.readdir(directoryPath, function (err, files) {    
                 // Handling error
                 if (err) {
                     rej(console.log('[ERROR] Unable to scan directory: ' + err));
                 }
     
-                // Listing all files using forEach
-                files.forEach(function (file) {
-                    configs.push(file);
-                });
-
-                res(configs);
+                res(files);
             });
         });
 
