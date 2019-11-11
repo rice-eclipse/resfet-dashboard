7af4b11310818f3bcc5a052f8534d06629f8377b
Berk Alp Yakici <berkalp.y@gmail.com>
2019-11-10 19:38:39 -0600

    Add idle timer

--- modules/sensor_logging.js
+++ modules/sensor_logging.js
@@ -1,3 +1,13 @@
+function getFormattedDate() {
+    var date = new Date();
+    var str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+
+    return str;
+}
+
 module.exports = {
-    
+    directory: "",
+    init: function() {
+        this.directory = getFormattedDate()
+    }
 }
