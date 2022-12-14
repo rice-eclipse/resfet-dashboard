a9633be6389092cfbc4632bba53067063dadeb26
Clayton Ramsey <claytonwramsey@gmail.com>
2022-12-13 18:18:23 -0600

    remove dangling UDP references

--- js/connection-status.js
+++ js/connection-status.js
@@ -2,28 +2,15 @@
 const { ipcRenderer } = require('electron');
 
 const bannerTCP = document.getElementById("bannerTCP");
-const bannerUDP = document.getElementById("bannerUDP");
 
 ipcRenderer.on('statusTCP', (event, arg) => {
-    bannerTCP.classList.remove("badge-warning");
-    bannerTCP.classList.remove("badge-danger");
-    bannerTCP.classList.remove("badge-success");
+  bannerTCP.classList.remove("badge-warning");
+  bannerTCP.classList.remove("badge-danger");
+  bannerTCP.classList.remove("badge-success");
 
-    if(arg) {
-      bannerTCP.classList.add("badge-success");
-    } else {
-      bannerTCP.classList.add("badge-danger");
-    }
-});
-
-ipcRenderer.on('statusUDP', (event, arg) => {
-  bannerUDP.classList.remove("badge-warning");
-  bannerUDP.classList.remove("badge-danger");
-  bannerUDP.classList.remove("badge-success");
-
-  if(arg) {
-    bannerUDP.classList.add("badge-success");
+  if (arg) {
+    bannerTCP.classList.add("badge-success");
   } else {
-    bannerUDP.classList.add("badge-danger");
+    bannerTCP.classList.add("badge-danger");
   }
 });
